33062dbad46304c6db4deeafdb6a0324e54e7fbd
Success Enehikhuere <enehikhueresuccess@gmail.com>
2023-11-16 12:37:32 +0100

    feat: add functionality to the components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { useEffect, useState } from "react";
 import{Loading} from './Loading'
 import {Tours} from './Tours'
 
+
+
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState (false);
   const [tours, setTours] = useState([])
+
+
+  const removeTour = (id) =>{
+    const newTour = tours.filter((tour)=>tour.id !== id)
+    setTours(newTour)
+  }
+
+ 
   
   const fetchData = async() =>{
     setIsLoading(true)
@@ -38,9 +48,24 @@ const App = () => {
   </h2>
  }
 
+ 
+ if(tours.length === 0){
   return(
     <main>
-      <Tours tours = {tours}/>
+      <div className="title">
+        <h2>No tours left</h2>
+        <button type='button' style={{marginTop:'2rem'}} className="btn" onClick={fetchData}>refresh</button>  
+      </div>
+    
+    </main>
+    
+  )
+ }
+
+
+  return(
+    <main>
+      <Tours tours = {tours} removeTour={removeTour} />
     </main>
   ) 
 };
